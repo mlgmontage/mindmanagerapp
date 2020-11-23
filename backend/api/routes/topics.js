07b5c566cc134811f05014b9f8325c5b11482ecc
mlgmontage <kettleofrassel1@gmail.com>
2020-11-23 10:39:25 +0500

    create topic route

--- backend/api/routes/topics.js
+++ backend/api/routes/topics.js
@@ -1,10 +1,23 @@
 const { Router } = require("express");
 const router = Router();
 const ds = require("../../connection");
+const topicSchema = require("../model/topicSchema");
 
 router.get("/", async (req, res) => {
   const topics = await ds.find({});
   res.json(topics);
+});
+
+router.post("/create", async (req, res) => {
+  const body = req.body;
+
+  if (!topicSchema.validate(topicSchema).errors) {
+    body.Date = new Date().toISOString();
+    const created = await ds.insert(body);
+    res.json(created);
+  } else {
+    res.status(400).json(topicSchema.validate(body).errors.message);
+  }
 });
 
 module.exports = router;
